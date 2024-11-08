2539ae853128a70421482ac9a822a39367f09777
kv-sub <kvsubramanian03@gmail.com>
2024-11-08 14:25:03 +0530

    Brooo!

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello, World!!!####!!\n');
-  res.end('Hiiii, Brooo!!!####!!\n');
 });
 
 server.listen(port, hostname, () => {
